a33450186e6a0802a9e8a9bd20e92f11d0061d47
YujunLuo <k4917552@gmail.com>
2025-06-14 08:06:06 +0800

    Delete blog directory

--- blog/admin_custom.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'created_at']  # 列表顯示的欄位
-    list_filter = ['category', 'created_at']           # 過濾器
-    search_fields = ['title', 'content']              # 搜尋欄位
-    date_hierarchy = 'created_at'                     # 日期導航
